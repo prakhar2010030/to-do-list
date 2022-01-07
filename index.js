e1999adc934cb56385a50b8e1de8ecbc296f455e
Prakhar2010030 <prakhar2010030kumar@gmail.com>
2022-01-07 12:34:53 +0530

    remove button
    added remove button using js

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
  function Add(){
+     var li=document.createElement("li");
      var note=document.getElementById("note").value;
+     var t=document.createTextNode(note);
+     li.appendChild(t);
      if(note==""){
          alert("Enter your note!!!");
      }
      else{
-         var li=document.createElement("li");
-        var t=document.createTextNode(note);
-         li.appendChild(t);
+         document.getElementById("ul").appendChild(li);
+    
         }
-        document.getElementById("ul").appendChild(li);
-       
+        document.getElementById("note").value="";
+        
+        var span = document.createElement("SPAN");
+        var rem=document.createTextNode("remove") ;
+        span.className="removebtn";
+        span.appendChild(rem);
+        li.appendChild(span);
         
 }
